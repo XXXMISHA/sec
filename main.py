67be2fb28a84ad4ce174b044ded0321d2c3d5bed
Misha <mbudk1n@yandex.ru>
2023-03-15 18:03:40 +0300

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+def sd(sec):
+    days = sec // (24 * 3600)
+    sec %= 24 * 3600
+    hours = sec // 3600
+    sec %= 3600
+    min = sec // 60
+    sec %= 60
+    print(f'{days}:{hours}:{min}:{sec}')
+
+
+sd(1000000)
